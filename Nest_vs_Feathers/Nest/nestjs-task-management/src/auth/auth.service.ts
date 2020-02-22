1a1d4aadf81ee2ff6abe4f55705a2b0d68d70ccd
Merlin <merlin199919@gmail.com>
2020-02-23 00:22:50 +0100

    login and signup

---
+++ Nest_vs_Feathers/Nest/nestjs-task-management/src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+    private jwtService: JwtService,
+  ) {}
+
+  async signUp(authCredentials: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.signUp(authCredentials);
+  }
+
+  async signIn(authCredentials: AuthCredentialsDto): Promise<{accessTokens: string}> {
+    const username = await this.userRepository.validateUserPassword(authCredentials);
+    console.log(username);
+
+    if (!username) {
+      throw new UnauthorizedException('Invalid Credentials');
+    }
+
+    const payload: JwtPayload = {username};
+    const accessTokens = await this.jwtService.sign(payload);
+
+    return {accessTokens};
+  }
+}
