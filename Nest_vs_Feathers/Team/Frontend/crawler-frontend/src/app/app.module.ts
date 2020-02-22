b6ecc15031ff2e91bf94fa36a2a3c3d842948779
GringoFabi <fabian15121998@googlemail.com>
2020-02-22 21:30:16 +0100

    NG generate: - module + component: show - service: scrapper - class: user Also added semantic-ui-offline.

--- Nest_vs_Feathers/Team/Frontend/crawler-frontend/src/app/app.module.ts
+++ Nest_vs_Feathers/Team/Frontend/crawler-frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ShowModule} from './show/show.module';
+import {ScrapperService} from './scrapper/scrapper.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShowModule
   ],
-  providers: [],
+  providers: [ScrapperService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
