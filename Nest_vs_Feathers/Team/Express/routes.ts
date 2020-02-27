53cbcbedf32d6f4a2586c5d6c84e5be5523af9a3
GringoFabi <fabian15121998@googlemail.com>
2020-02-27 20:54:12 +0100

    User credentials validation.

--- Nest_vs_Feathers/Team/Express/routes.ts
+++ Nest_vs_Feathers/Team/Express/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import joi from 'joi';
 
 const app = express.Router();
 
 export { app as routes };
 
 const users = [];
+
 app.get('/', ((req, res) => {
     res.send('hello world!')
 }));
@@ -18,6 +20,16 @@ app.get('/users/:name', ((req, res) => {
 }));
 
 app.post('/users', ((req, res) => {
+    const schema = {
+        name: joi.string().min(1).required(),
+        password: joi.string().min(1).required()
+    };
+    const result = joi.validate(req.body, schema);
+
+    if (result.error) {
+        res.status(400).send(result.error);
+        return;
+    }
     const user = {
         name: req.body.name,
         password: req.body.password
@@ -27,7 +39,8 @@ app.post('/users', ((req, res) => {
 }));
 
 app.delete('/users/:name', ((req, res) => {
-    res.send({})
+    users.splice(users.indexOf(req.body),1);
+    res.send({});
 }));
 
 app.patch('/users/:name', ((req, res) => {
